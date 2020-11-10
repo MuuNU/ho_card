134086b3ef2b4353c2a830ddc292a52f7036387f
Admin <Admin@ekb1.restime911.ru>
2020-11-10 10:45:03 +0500

    base

--- app/rc_api.py
+++ app/rc_api.py
@@ -1,14 +1,34 @@
 import requests
 from pprint import pprint
 import json
-def login():
-    data = {'user': 'testA',
-    'password': 'testAdmin123'}
-    response = requests.post('https://chat-7time.ru/api/v1/login', data=data)
+def login(uname, upass):
+    details = {'user': uname,
+    'password': upass}
+    response = requests.post('https://chat-7time.ru/api/v1/login', data=details)
     data = (json.loads(response.text))['data']
-    pprint(data['authToken'])
-    pprint(data['me']['_id'])
+    token = (data['authToken'])
+    id = (data['me']['_id'])
+
+    return token, id
+
+def channels_list(token, id):
+    headers = { 'X-Auth-Token': token,
+                'X-User-Id': id,}
+    response = requests.get('https://chat-7time.ru/api/v1/groups.list', headers=headers)
+    data = (json.loads(response.text))
+    gname = []
+    gid = []
+
+    for i in range(data['count']):
+        gname.append(data['groups'][i]['name'])
+        gid.append(data['groups'][i]['_id'])
+
+    return gname, gid
+
 
 
 if __name__ == '__main__':
-    login()
+    token, id = login('testA', 'testAdmin123')
+    gname, gid = channels_list(token, id)
+    print(gname)
+    print(gid)
